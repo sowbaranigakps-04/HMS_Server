929f659e39fba7b7b6e235df637b7233102d5bfc
sowbaranigakps-04 <sowbaranigakps04@gmail.com>
2025-05-07 09:57:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,12 +33,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Could not connect to MongoDB', err));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))  
+  .catch((err) => console.error('Could not connect to MongoDB', err));
 
 // Routes
 app.use('/api/signup', signupRoutes);
