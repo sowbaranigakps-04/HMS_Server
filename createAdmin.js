622c2e24870aaea8c6ff4e05885f0f305017b2c6
sowbaranigakps-04 <sowbaranigakps04@gmail.com>
2025-05-07 09:56:52 +0530

    Update createAdmin.js

--- createAdmin.js
+++ createAdmin.js
@@ -4,12 +4,10 @@ import Admin from './models/Admin.js';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Could not connect to MongoDB', err));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))  
+  .catch((err) => console.error('Could not connect to MongoDB', err));
 
 async function createAdmin() {
   const admin = new Admin({
